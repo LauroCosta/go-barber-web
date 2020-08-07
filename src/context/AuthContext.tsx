03cb2a574c0b83c033079210430c0486fa553251
Lauro Costa <joaolauro94@gmail.com>
2020-08-06 23:50:39 -0300

    api

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,21 @@
-import React, { createContext } from 'react';
+import React, { createContext, useCallback } from 'react';
 
 interface AuthContextData {
   name: string;
+  signIn(): void;
 }
 
-const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const signIn = useCallback(() => {
+    console.log('signin');
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ name: 'lauro' }}>
+    <AuthContext.Provider value={{ name: 'lauro', signIn }}>
       {children}
     </AuthContext.Provider>
   );
