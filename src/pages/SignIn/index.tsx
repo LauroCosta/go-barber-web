de9981ab8b7bdf8d53fab2b8fc1d57576b69a4ff
Lauro Costa <joaolauro94@gmail.com>
2020-08-08 14:00:51 -0300

    animate toast

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -53,7 +53,7 @@ const SignIn: React.FC = () => {
         }
 
         addToast({
-          type: 'success',
+          type: 'error',
           title: 'Erro na autenticação',
           description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
         });
